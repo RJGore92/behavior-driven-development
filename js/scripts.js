bdc4a389024900f22f2801186c120f6e53ba7e44
Robert Gore <DarkburstGX@gmail.com>
2019-01-16 13:46:28 -0800

    Initial project commit for unit 3.2: Behavior Driven Development.  Contains draft readme for assignment, Leap Year Detector example, and blank html template, scripts, and css for the Pig Latin Translator assignment.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// business logic
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
